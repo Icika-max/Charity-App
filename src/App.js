5bba6e5b60780103d9ac393cd93c53cec51297d3
ianK-code <kian99564@gmail.com>
2023-01-26 14:04:24 +0300

    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom'
-import './App.css';
-import Char from './components/charity/Char';
-import Home from './components/LandingPage/Home'
-import Nav from './components/Nav'
-import List from './components/charity/List'
-
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Char from "./components/charity/Char";
+import Home from "./components/LandingPage/Home";
+import Nav from "./components/Nav";
+import List from "./components/charity/List";
 
 function App() {
   return (
     <div className="App">
-      <Nav>
+      <BrowserRouter>
+        <Nav />
         <Routes>
-          <Route path='/charity' >
-            <Char/>
-          </Route>
-          <Route path='list'>
-            <List />
-          </Route>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          </Routes>
-      </Nav>
-      
+          <Route path="/charity" element={<Char />} />
+          <Route path="list" element={<List />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
